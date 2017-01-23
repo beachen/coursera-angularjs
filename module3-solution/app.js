5ef83930a000e70c44f1f559dd9ebbcf5d303328
Anders Strand <andersstrand.se@gmail.com>
2017-01-23 15:18:15 +0100

    Implemented search

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -15,31 +15,50 @@ angular.module('NarrowItDownApp', [])
   **/
   function NarrowItDownController($scope, MenuSearchService) {
       var ctrl = this;
-
       var searchStr = "";
+
 
       ctrl.search = function (searchTerm) {
           console.log("Searching for:" + searchTerm);
-          ctrl.items =  MenuSearchService.getMatchedMenuItems(searchTerm);
-          console.log(ctrl.items);
+          MenuSearchService.getMatchedMenuItems(searchTerm).
+          then(function (res) {
+
+              ctrl.found = res;
+              console.log("Found:" + res.length);
+          });
       }
 
 
   };
+
+  MenuSearchService.$inject = ['$http'];
   /**
   * Service
   *
   */
-  function MenuSearchService() {
+  function MenuSearchService($http) {
     var service = this;
 
+
+    /** Call external service and filter by search term. **/
     service.getMatchedMenuItems = function (searchTerm) {
-    // https://davids-restaurant.herokuapp.com/menu_items.json
-        console.log("searchTerm:" + searchTerm);
-        return {name:"item1"};
+      var foundItems = [];
+      console.log("About to call service to load data:" + searchTerm);
+      return $http({
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"})
+        .then(function (result) {
+          // Process
+          result.data.menu_items.forEach(function(e) {
+            if(e.description.includes(searchTerm)){
+                console.log(e);
+                foundItems.push(e);
+            }
+            });
+          console.log("Returning number of foundItems" + foundItems.length);
+          return foundItems;
+      });
     }
-
-
   };
 
 })();
