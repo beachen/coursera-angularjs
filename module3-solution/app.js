e3b34363765037250df57a41e800a4b7feb693c2
Anders Strand <andersstrand.se@gmail.com>
2017-01-24 14:44:39 +0100

    Added directive

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,7 +3,33 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+
+function FoundItemsDirective() {
+  /** Directive definition object. **/
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+
+function FoundItemsDirectiveController() {
+    var list = this;
+
+}
+
+
 
   // Inject Scope
   NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
@@ -14,10 +40,13 @@ angular.module('NarrowItDownApp', [])
   *
   **/
   function NarrowItDownController($scope, MenuSearchService) {
+
       var ctrl = this;
       var searchStr = "";
+      ctrl.found = [];
 
 
+      /** Search for menu items. **/
       ctrl.search = function (searchTerm) {
           console.log("Searching for:" + searchTerm);
           MenuSearchService.getMatchedMenuItems(searchTerm).
@@ -26,9 +55,16 @@ angular.module('NarrowItDownApp', [])
               ctrl.found = res;
               console.log("Found:" + res.length);
           });
+      };
+      /** Remove one item from the items list**/
+      ctrl.removeItem = function (index) {
+          console.log("Remove!" + index);
+          // Remove the item
+          ctrl.found.splice(index, 1);
+      };
+      ctrl.nothingFound = function () {
+          return ctrl.found.length === 0;
       }
-
-
   };
 
   MenuSearchService.$inject = ['$http'];
