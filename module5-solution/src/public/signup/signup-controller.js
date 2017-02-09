86178e24819ea6974b09df4a285a0c2202afdd87
Anders Strand <andersstrand.se@gmail.com>
2017-02-09 15:44:19 +0100

    added save message

--- module5-solution/src/public/signup/signup-controller.js
+++ module5-solution/src/public/signup/signup-controller.js
@@ -6,15 +6,22 @@
     function SignupController() {
       var $ctrl = this;
       $ctrl.user = {};
-
+      $ctrl.msg = null;
       console.log("Singup controller");
         $ctrl.signUp = function () {
 
             console.log("Hello:" + $ctrl.user.firstName);
             if($ctrl.user.favorite != null){
                 // Well, let's check if it is available
-
+                $ctrl.msg = 'Your data has been saved ' + $ctrl.user.firstName;
             }
-        }
+            else{
+              $ctrl.msg = 'No valid favorite menu number. Data is NOT saved';
+            }
+        };
+        $ctrl.showMsg = function () {
+
+          return $ctrl.msg != null;
+        };
     }
 })();
